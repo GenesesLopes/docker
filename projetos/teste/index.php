0207028e6693e0c31adcaf89683e8f10dedca735
genesesLopes <genesesslopes@gmail.com>
2020-05-29 02:22:02 -0300

    padrao nginx e apache

---
+++ projetos/teste/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página inicial</title>
+</head>
+<body>
+    <h1>Teste com virtualHost PHP!</h1>
+</body>
+</html>
